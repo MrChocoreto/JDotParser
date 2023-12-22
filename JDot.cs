52e509eb1735b4b21af95f5db37ba16cdf1a5844
Mr_Chocoreto <chocoretostonus@gmail.com>
2023-12-22 03:38:41 -0600

    Loading Primitives

--- JDot.cs
+++ JDot.cs
@@ -7,6 +7,7 @@
             return new JDotSave().ToDataFile(Class);
         }
 
+
         public static T FileToClass<T>(string Data, bool IsPath = false) {
             return new JDotLoad().ToDataClass<T>(Data, IsPath);
         }
